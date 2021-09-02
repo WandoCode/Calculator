da2953786fcfa665db15d880130c6909b9c49b39
Maxime <maxime_chirez@hotmail.com>
2021-09-03 00:20:43 +0200

    Final commit

--- script/script.js
+++ script/script.js
@@ -57,34 +57,63 @@ const buttonPressed = function(e) {
     else if (e.target.id == "clear") {
         arrayAns = [];
         screen.innerHTML = 0;
-        calcul.operator = "";
+        calcul.operator = "none";
         calcul.lastNum = 0;
         calcul.currentNum = 0;
         return;
     } 
+    else if (e.target.id == "plus-less") {
+        calcul.lastNum = calcul.lastNum * -1;
+    }
     else {
 
         if (calcul.operator == "none"){
             calcul.lastNum = arrayAns.join("");
         } 
-        /* Ajouter les cas de % et +/-  dans des else if*/
         else if (calcul.operator != ""){
-
             calcul.currentNum = arrayAns.join("");
             calcul.lastNum = operate(calcul.lastNum, calcul.currentNum, calcul.operator);
         }
 
         arrayAns = [];
         calcul.operator = e.target.id == "equal" ? "" : e.target.id;
-        screen.innerHTML = calcul.lastNum;
+        screen.innerHTML = formatForScreen(calcul.lastNum);
     }
 };
 
+let formatForScreen = function(num) {
+    const strNum = num.toString();
+    if (strNum.length >= 12 && strNum.includes(".")) {
+        num = Math.round(num * (10 ** 12))/ (10**12);
+    }
+    else if (strNum.length >= 12) {
+        num = NaN; /*Number too hight*/
+    }
+
+    return num;
+}
+
+
 const addToScreen = function(num) {
-    if (num != 0 && arrayAns[0] != 0) {
+    if (arrayAns.length > 12) {
+        return;
+    }
+    else if (num == "." && arrayAns.includes(".")){
+        return;
+    }
+    else if (arrayAns.length > 0){
         arrayAns.push(num);
-        screen.innerHTML = arrayAns.join("");
-    }   
+    }
+    else if (num == 0) {
+        return;
+    }
+    else if (num == "."){
+        arrayAns = ["0", "."];
+    }
+    else {
+        arrayAns.push(num);
+    }
+    screen.innerHTML = arrayAns.join("");
 }
 
 
